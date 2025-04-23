c61b643918c2489acd62f631a19781c1ac60318a
Shp1nat <sergeev.stanislav03@gmail.com>
2025-04-23 07:59:43 +0300

    based views separated to user/admins

--- src/layouts/ProfileLayout.vue
+++ src/layouts/ProfileLayout.vue
@@ -3,14 +3,12 @@
 </template>
 
 <script>
-import userProfile from '@/views/userDashboard/ProfileView.vue'
-import adminProfile from '@/views/adminDashboard/ProfileView.vue'
+import profile from '@/views/commonDashboard/ProfileView.vue'
 
 export default {
   computed: {
     currentComponent() {
-      const isAdmin = localStorage.getItem('status') === 'admin'
-      return isAdmin ? adminProfile : userProfile
+      return profile
     }
   }
 }
