0ef6e3211c97a66eab588cdfe690159b240be9c4
Shp1nat <sergeev.stanislav03@gmail.com>
2025-05-19 18:54:26 +0300

    typo fix

--- src/views/userDashboard/VacanciesView.vue
+++ src/views/userDashboard/VacanciesView.vue
@@ -42,7 +42,7 @@
       </div>
 
       <div v-if="!isLoading && !vacancies.length && !errorMessage" class="empty-table-message">
-        Ваш список откликов выбранного типа пуст.
+        Список откликов выбранного типа пуст.
       </div>
 
       <div
